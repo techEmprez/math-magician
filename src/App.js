6d2664673dae19fbd65fa8a65658e9d564a37aa9
techEmprez <choiceosobor@gmail.com>
2022-09-09 04:09:06 +0100

    add styling to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <div className="App" />
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Calculator />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
