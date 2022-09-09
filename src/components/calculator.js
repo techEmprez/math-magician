89fed05f139fb9b71a388533b4a6f210179a068d
techEmprez <choiceosobor@gmail.com>
2022-09-09 23:52:29 +0100

    refactor with hooks

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -3,54 +3,49 @@ import calculate from './logic/calculate';
 import './calculator.css';
 
 const Calculator = () => {
-  const [numbers, setState] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+  const [obj, setobj] = useState({ total: 0, next: null, operation: null });
 
-  const afterClick = (e) => {
-    const buttonName = e.target.innerText;
-    setState(calculate({
-      ...numbers,
-    }, buttonName));
+  const handleClickEvent = (e) => {
+    setobj(calculate(obj, e.target.id));
   };
 
-  const { total, operation, next } = numbers;
-
-  const result = (
-    <p>
-      {total}
-      {' '}
-      {operation}
-      {' '}
-      {next}
-    </p>
-  );
-
+  const { total, next, operation } = obj;
   return (
-    <div className="calculator-wrapper">
-      <div className="cal-input">{result}</div>
-      <div className="grid-container">
-        <div className="grid-item1"><button type="button" className="button" onClick={afterClick}>AC</button></div>
-        <div className="grid-item2"><button type="button" className="button" onClick={afterClick}>+/-</button></div>
-        <div className="grid-item3"><button type="button" className="button" onClick={afterClick}>%</button></div>
-        <div className="grid-item4" id="right-side"><button type="button" className="button" onClick={afterClick}>÷</button></div>
-        <div className="grid-item5"><button type="button" className="button" onClick={afterClick}>7</button></div>
-        <div className="grid-item6"><button type="button" className="button" onClick={afterClick}>8</button></div>
-        <div className="grid-item7"><button type="button" className="button" onClick={afterClick}>9</button></div>
-        <div className="grid-item8" id="right-side"><button type="button" className="button" onClick={afterClick}>x</button></div>
-        <div className="grid-item9"><button type="button" className="button" onClick={afterClick}>4</button></div>
-        <div className="grid-item10"><button type="button" className="button" onClick={afterClick}>5</button></div>
-        <div className="grid-item11"><button type="button" className="button" onClick={afterClick}>6</button></div>
-        <div className="grid-item12" id="right-side"><button type="button" className="button" onClick={afterClick}>-</button></div>
-        <div className="grid-item13"><button type="button" className="button" onClick={afterClick}>1</button></div>
-        <div className="grid-item14"><button type="button" className="button" onClick={afterClick}>2</button></div>
-        <div className="grid-item15"><button type="button" className="button" onClick={afterClick}>3</button></div>
-        <div className="grid-item16" id="right-side"><button type="button" className="button" onClick={afterClick}>+</button></div>
-        <div className="grid-item17"><button type="button" className="button" onClick={afterClick}>0</button></div>
-        <div className="grid-item18"><button type="button" className="button" onClick={afterClick}>.</button></div>
-        <div className="grid-item19" id="right-side"><button type="button" className="button" onClick={afterClick}>=</button></div>
+    <div className="container">
+      <div className="display-result">
+        <span>{total}</span>
+        <span>{operation}</span>
+        <span>{next}</span>
+      </div>
+      <div className="row-one grid">
+        <button type="button" onClick={handleClickEvent} id="AC" className="col">Ac</button>
+        <button type="button" onClick={handleClickEvent} id="+/-" className="col">+/-</button>
+        <button type="button" onClick={handleClickEvent} id="%" className="col">%</button>
+        <button type="button" onClick={handleClickEvent} id="÷" className="col">÷</button>
+      </div>
+      <div className="row-two grid">
+        <button type="button" onClick={handleClickEvent} id="7" className="col">7</button>
+        <button type="button" onClick={handleClickEvent} id="8" className="col">8</button>
+        <button type="button" onClick={handleClickEvent} id="9" className="col">9</button>
+        <button type="button" onClick={handleClickEvent} id="*" className="col">*</button>
+      </div>
+      <div className="row-three grid">
+        <button type="button" onClick={handleClickEvent} id="4" className="col">4</button>
+        <button type="button" onClick={handleClickEvent} id="5" className="col">5</button>
+        <button type="button" onClick={handleClickEvent} id="6" className="col">6</button>
+        <button type="button" onClick={handleClickEvent} id="-" className="col">-</button>
+      </div>
+      <div className="row-four grid">
+        <button type="button" onClick={handleClickEvent} id="1" className="col">1</button>
+        <button type="button" onClick={handleClickEvent} id="2" className="col">2</button>
+        <button type="button" onClick={handleClickEvent} id="3" className="col">3</button>
+        <button type="button" onClick={handleClickEvent} id="+" className="col">+</button>
+      </div>
+      <div className="row-five grid">
+        <div type="button" className="col div-col" />
+        <button type="button" onClick={handleClickEvent} id="0" className="col">0</button>
+        <button type="button" onClick={handleClickEvent} id="." className="col">.</button>
+        <button type="button" onClick={handleClickEvent} id="=" className="col">=</button>
       </div>
     </div>
   );
