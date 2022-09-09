5462cf60daceed58c2b0860a4bfbbde8db63d194
techEmprez <choiceosobor@gmail.com>
2022-09-09 04:40:06 +0100

    create a logic folder

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,36 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from './logic/calculate';
 import './calculator.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
-  render() {
-    return (
-      <div className="calculator-wrapper">
-        <input className="cal-input" placeholder="0" />
-        <div className="grid-container">
-          <div className="grid-item1"><button type="button" className="button">AC</button></div>
-          <div className="grid-item2"><button type="button" className="button">+/-</button></div>
-          <div className="grid-item3"><button type="button" className="button">%</button></div>
-          <div className="grid-item4" id="right-side"><button type="button" className="button">+</button></div>
-          <div className="grid-item5"><button type="button" className="button">7</button></div>
-          <div className="grid-item6"><button type="button" className="button">8</button></div>
-          <div className="grid-item7"><button type="button" className="button">9</button></div>
-          <div className="grid-item8" id="right-side"><button type="button" className="button">x</button></div>
-          <div className="grid-item9"><button type="button" className="button">4</button></div>
-          <div className="grid-item10"><button type="button" className="button">5</button></div>
-          <div className="grid-item11"><button type="button" className="button">6</button></div>
-          <div className="grid-item12" id="right-side"><button type="button" className="button">-</button></div>
-          <div className="grid-item13"><button type="button" className="button">1</button></div>
-          <div className="grid-item14"><button type="button" className="button">2</button></div>
-          <div className="grid-item15"><button type="button" className="button">3</button></div>
-          <div className="grid-item16" id="right-side"><button type="button" className="button">+</button></div>
-          <div className="grid-item17"><button type="button" className="button">0</button></div>
-          <div className="grid-item18"><button type="button" className="button">.</button></div>
-          <div className="grid-item19" id="right-side"><button type="button" className="button">=</button></div>
-        </div>
+const Calculator = () => {
+  const [numbers, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const afterClick = (e) => {
+    const buttonName = e.target.innerText;
+    setState(calculate({
+      ...numbers,
+    }, buttonName));
+  };
+
+  const { total, operation, next } = numbers;
+
+  const result = (
+    <p>
+      {total}
+      {' '}
+      {operation}
+      {' '}
+      {next}
+    </p>
+  );
+
+  return (
+    <div className="calculator-wrapper">
+      <div className="cal-input">{result}</div>
+      <div className="grid-container">
+        <div className="grid-item1"><button type="button" className="button" onClick={afterClick}>AC</button></div>
+        <div className="grid-item2"><button type="button" className="button" onClick={afterClick}>+/-</button></div>
+        <div className="grid-item3"><button type="button" className="button" onClick={afterClick}>%</button></div>
+        <div className="grid-item4" id="right-side"><button type="button" className="button" onClick={afterClick}>÷</button></div>
+        <div className="grid-item5"><button type="button" className="button" onClick={afterClick}>7</button></div>
+        <div className="grid-item6"><button type="button" className="button" onClick={afterClick}>8</button></div>
+        <div className="grid-item7"><button type="button" className="button" onClick={afterClick}>9</button></div>
+        <div className="grid-item8" id="right-side"><button type="button" className="button" onClick={afterClick}>x</button></div>
+        <div className="grid-item9"><button type="button" className="button" onClick={afterClick}>4</button></div>
+        <div className="grid-item10"><button type="button" className="button" onClick={afterClick}>5</button></div>
+        <div className="grid-item11"><button type="button" className="button" onClick={afterClick}>6</button></div>
+        <div className="grid-item12" id="right-side"><button type="button" className="button" onClick={afterClick}>-</button></div>
+        <div className="grid-item13"><button type="button" className="button" onClick={afterClick}>1</button></div>
+        <div className="grid-item14"><button type="button" className="button" onClick={afterClick}>2</button></div>
+        <div className="grid-item15"><button type="button" className="button" onClick={afterClick}>3</button></div>
+        <div className="grid-item16" id="right-side"><button type="button" className="button" onClick={afterClick}>+</button></div>
+        <div className="grid-item17"><button type="button" className="button" onClick={afterClick}>0</button></div>
+        <div className="grid-item18"><button type="button" className="button" onClick={afterClick}>.</button></div>
+        <div className="grid-item19" id="right-side"><button type="button" className="button" onClick={afterClick}>=</button></div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
