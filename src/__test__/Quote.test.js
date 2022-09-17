8af8019b47fafc68c0874da370e0d7ec0fbd4620
techEmprez <choiceosobor@gmail.com>
2022-09-17 12:16:36 +0100

    fixing testing configuration

--- src/__test__/Quote.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Quote from '../components/Quote';
-
-describe('SHould Quote component render', () => {
-  test('Renders quote', () => {
-    render(<Quote />);
-  });
-});
